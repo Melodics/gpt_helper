3f2e82908479759585b15e67b0e65005d9c22b50
Vincent Chow <vincent@melodics.com>
2023-05-04 11:13:33 +1200

    Attempt to fix bot replying "Sure, take your time."
    It seems that the completions API makes note of the 'text' field.
    
    Replace hardcoded "Before I answer..." with the actual text from the
    @app_mention event, to avoid the completions API thinking it should
    respond to "Before I answer..."

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ def handle_app_mention_events(event, say: Say, logger, body):
     greeting = random.choice(greeting_messages)
     prompt_id = say(
         channel=channel,
-        text="Before I answer...",
+        text=f"{event['text']}",
         thread_ts=thread_ts,
         # See https://app.slack.com/block-kit-builder/ for use of blocks
         blocks=[
